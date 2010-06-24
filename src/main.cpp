d20ab463051c1c77ed492db2b11b02903505adfd
Sergey Borisov <borisovs@gmail.com>
2010-06-24 11:53:58 +0400

    Fix.

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ int main (int argc, char *argv[])
 {
     QApplication app(argc, argv);
     app.setWindowIcon(QIcon(":/bugs/resources/ladybug.png"));
-
+    app.setApplicationName(QString("BugHunter"));
     GraphicsView gv;
 
     gv.show();
